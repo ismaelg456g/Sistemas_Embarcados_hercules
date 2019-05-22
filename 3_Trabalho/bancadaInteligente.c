a4bfe4924a24837c4759a5df510a72d5a5b15751
Hércules Ismael <ismael-456@hotmail.com>
2019-05-22 13:03:40 -0300

    dsreader para txt

---
+++ 3_Trabalho/bancadaInteligente.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <signal.h>
+
+
+int main(){
+  pid_t scanner;
+
+  // scanner=fork();
+
+  // if(scanner==0){
+    system("./dsreader > cadastro.txt");
+  // }else{
+  //
+  // }
+
+
+  return 0;
+}
