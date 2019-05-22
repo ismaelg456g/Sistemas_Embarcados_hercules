d0b1ad2bcf68775eb388ae381c06be3e5fed487c
Hércules Ismael <ismael-456@hotmail.com>
2019-05-22 13:24:56 -0300

    Projeto final dsreader processo

--- 3_Trabalho/bancadaInteligente.c
+++ 3_Trabalho/bancadaInteligente.c
@@ -5,17 +5,25 @@
 #include <sys/types.h>
 #include <signal.h>
 
+pid_t scanner;
+
+void mataDsreader(){
+  kill(scanner, SIGINT);
+}
 
 int main(){
-  pid_t scanner;
 
-  // scanner=fork();
 
-  // if(scanner==0){
+  signal(SIGALRM, mataDsreader);
+  scanner=fork();
+
+
+  if(scanner==0){
     system("./dsreader > cadastro.txt");
-  // }else{
-  //
-  // }
+  }else{
+    alarm(15);
+    pause();
+  }
 
 
   return 0;
